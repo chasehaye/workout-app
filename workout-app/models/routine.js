e4751ef84a43e6ade0c9416b47235c779a2254e1
chasehaye <chasehayejoc@gmail.com>
2023-11-28 18:48:03 -0700

    .

--- workout-app/models/routine.js
+++ workout-app/models/routine.js
@@ -4,7 +4,6 @@ const Schema = mongoose.Schema;
 const routineSchema = new Schema({
     name: {type: String, required: true},
     description: {type: String, required: true},
-
 });
 
 module.exports = mongoose.model('Routine', routineSchema)
--- workout-app/models/routineCycle.js
+++ workout-app/models/routineCycle.js
@@ -4,7 +4,11 @@ const Schema = mongoose.Schema;
 const routineCycleSchema = new Schema({
     name: {type: String, required: true},
     description: {type: String, required: true},
-
+    routinesArr: [],
+    routines: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Routine'
+    }
 });
 
 module.exports = mongoose.model('RoutineCycle', routineCycleSchema)
